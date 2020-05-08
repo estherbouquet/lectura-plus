f23ecdec58f46e899e07482c9e82d1b23bcb0e71
estherbouquet <ebouquet@orange.fr>
2020-05-08 21:27:50 +0200

    Revert "adapting files for raspberry environment"
    This reverts commit 0a29621c42235f031309b9098cc2faaa96c2cd21.

--- copy_from_usb.sh
+++ copy_from_usb.sh
@@ -1,24 +1,17 @@
 # Mettre tous les fichiers dans un sous dossier de la clé usb (genre /input)
 
 # usb_folder=/media/NOM_USB/input
-#file='/Users/leabelzunces/code/lectura-plus/test.txt'
-file='/home/pi/Documents/lectura-plus-master/test.txt'
+file='/Users/leabelzunces/code/lectura-plus/test.txt'
 
 # On crée les trois dossiers
-#mkdir '/Users/leabelzunces/code/lectura-plus/input'
-#mkdir '/Users/leabelzunces/code/lectura-plus/output'
-#mkdir '/Users/leabelzunces/code/lectura-plus/images'
-mkdir '/home/pi/Documents/lectura-plus-master/input'
-mkdir '/home/pi/Documents/lectura-plus-master/output'
-mkdir '/home/pi/Documents/lectura-plus-master/images'
+mkdir '/Users/leabelzunces/code/lectura-plus/input'
+mkdir '/Users/leabelzunces/code/lectura-plus/output'
+mkdir '/Users/leabelzunces/code/lectura-plus/images'
 
 # On les stocke dans des variables
-#input_folder='/Users/leabelzunces/code/lectura-plus/input'
-#output_folder='/Users/leabelzunces/code/lectura-plus/output'
-#images_folder='/Users/leabelzunces/code/lectura-plus/images'
-input_folder='/home/pi/Documents/lectura-plus-master/input'
-output_folder='/home/pi/Documents/lectura-plus-master/output'
-images_folder='home/pi/Documents/lectura-plus-master/images'
+input_folder='/Users/leabelzunces/code/lectura-plus/input'
+output_folder='/Users/leabelzunces/code/lectura-plus/output'
+images_folder='/Users/leabelzunces/code/lectura-plus/images'
 
 # On copie les fichiers de la clé usb dans le dossier input
 cp $file $input_folder
